a5b8720d63b54346ce85b30e1b35c80870abd372
Danila Sosnov <dsosnov2009@gmail.com>
2025-03-11 17:48:08 +0300

    Add project files.

---
+++ Task01/logic.cpp
@@ -0,0 +1,51 @@
+﻿// Сумма элементов [The sum of elements]
+//
+// Необходимо разработать функцию, которая вычисляет 
+// сумму элементов массива, расположенных между первыми
+// максимальным по модулю и минимальным по модулю элементами.
+
+#include "logic.h"
+
+
+int sum_between_first_min_max(int* array, int size)
+{
+	
+	int maxIndex = 0;
+	int minIndex = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (abs(array[i]) > abs(array[maxIndex]))
+		{
+			maxIndex = i;
+		}
+
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (abs(array[i]) < abs(array[minIndex]))
+		{
+			minIndex = i;
+		}
+	}
+
+	if (maxIndex > minIndex)
+	{
+		int t = minIndex;
+		minIndex = maxIndex;
+		maxIndex = t;
+	}
+
+	int sum = 0;
+
+	for (int i = maxIndex + 1; i < minIndex; i++)
+	{
+		sum += array[i];
+	}
+
+
+	
+
+	return sum;
+}
