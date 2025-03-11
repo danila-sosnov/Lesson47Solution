5f1591bd5b0c1746907e6b9123a98d1496009ae6
Danila Sosnov <dsosnov2009@gmail.com>
2025-03-11 18:40:04 +0300

    Solve task02 task03

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -3,3 +3,29 @@
 //
 // Дан вектор целых чисел. Необходимо разработать функцию, 
 // которая вычисляет среднее арифметическое ненулевых элементов вектора.
+
+#include "logic.h"
+
+
+double calculate_arithmetical_mean_of_nonsero_elements(int* array, int size)
+{
+
+	double sum = 0;
+	int count = 0;
+	
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != 0)
+		{
+			sum += array[i];
+			count++;
+		}
+	}
+
+	
+
+	return count == 0 ? 0 : sum / count;
+
+
+}
--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -4,3 +4,42 @@
 // Дан вектор натуральных чисел. Необходимо разработать 
 // функцию, которая вычисляет среднее рифметическое элементов
 // вектора без учета минимального и максимального элементов.
+
+double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size)
+{
+	int max = 0;
+	int min = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+		{
+			max = array[i];
+		}
+
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < min)
+		{
+			min = array[i];
+		}
+	}
+
+	double sum = 0;
+	int count = 0;
+	
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != max && array[i] != min)
+		{
+			sum += array[i];
+			count++;
+		}
+	}
+
+
+	return count == 0 ? 0 : sum / count;
+}
